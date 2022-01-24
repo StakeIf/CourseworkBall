e11f2af0a3972bf9e60c6f077f7bd2335e2a8b81
StakeIf <krasnov2002kolya@mail.ru>
2022-01-24 09:29:24 +0700

    Game rabotaet

--- CourseworkBall/Game.Designer.cs
+++ CourseworkBall/Game.Designer.cs
@@ -3,14 +3,14 @@
     partial class Game
     {
         /// <summary>
-        /// Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +20,53 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(446, 490);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Взлетать!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Game";
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(590, 590);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Game";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Flappy Ball";
+            this.Load += new System.EventHandler(this.Game_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
     }
 }
+
--- CourseworkBall/Game.cs
+++ CourseworkBall/Game.cs
@@ -7,14 +7,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+
+//Добавить класс Точка для наследования координат
 
 namespace CourseworkBall
 {
     public partial class Game : Form
     {
+        Player ball;
+        TheWall upperWall;
+        TheWall lowerWall;
+      
+
+        float gravity;
+
+        //Конструктор
         public Game()
         {
             InitializeComponent();
+            timer1.Interval = 10;
+            timer1.Tick += new EventHandler(Update);
+            Init();
+            Invalidate();
+        }
+
+        //Расстановка объектов на экране
+        public void Init()
+        {
+            ball = new Player(200, 200);
+            upperWall = new TheWall(650, -200);
+            lowerWall = new TheWall(650, 400, true);
+     
+
+            gravity = 0;
+            this.Text = "Flappy ball Score: 0";
+            timer1.Start();
+        }
+
+        //Отрисовка объектов
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            graphics.DrawImage(ball.figureImage, ball.x, ball.y, ball.sizeX, ball.sizeY);
+            graphics.DrawImage(upperWall.figureImage, upperWall.x, upperWall.y, upperWall.sizeX, upperWall.sizeY);
+            graphics.DrawImage(lowerWall.figureImage, lowerWall.x, lowerWall.y, lowerWall.sizeX, lowerWall.sizeY);
+  
+        }
+
+        //Запуск игры заново
+        private void Update(object sender, EventArgs e)
+        {
+            //Если шар упал ниже поля
+            if (ball.y > 600)
+            {
+                if (ball.Pop(timer1))
+                {
+                    Thread.Sleep(1000);
+                    Init();
+                }
+            }
+            //Если шар врезался в стену
+            if (ball.Collide(upperWall) || ball.Collide(lowerWall))
+            {
+                if (ball.Pop(timer1))
+                {
+                    Thread.Sleep(1000);
+                    Init();
+                }
+            }
+
+            ball.Gravity(ref gravity);
+
+            if (ball.GetAlive())
+                MoveWalls();
+
+            Invalidate();
+        }
+
+        //Движение стен и монеты
+        private void MoveWalls()
+        {
+            upperWall.Move();
+            lowerWall.Move();
+            upperWall.CreateNewWall(upperWall, lowerWall, ball);
+        }
+
+        //Нажатие на кнопку для полета
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ball.Flight(ref gravity);
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
